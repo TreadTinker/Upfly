bdab9f9867316e10c3f1a1da3c0e17914123f2e1
Tatsu <68650923+TreadTinker@users.noreply.github.com>
2022-05-18 14:04:42 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let userInfo = {};
+
+userInfo.name = 'Вася';
+userInfo.age = 18;
+ console.log(userInfo);
+
+ userInfo.name = 'Лена';
+
+ console.log(userInfo);
+
+ delete userInfo.name;
+
+ console.log(userInfo);
