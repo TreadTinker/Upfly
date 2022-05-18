84b089b8ae8491b966077dc319bcb3aa84d301e2
Tatsu <68650923+TreadTinker@users.noreply.github.com>
2022-05-18 15:10:26 +0300

    1s

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
-let userInfo = {};
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
 
-userInfo.name = 'Вася';
-userInfo.age = 18;
- console.log(userInfo);
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+      popup.classList.add('active'); // И для самого окна
+  })
+});
 
- userInfo.name = 'Лена';
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popupBg.classList.remove('active'); // Убираем активный класс с фона
+  popup.classList.remove('active'); // И с окна
+});
 
- console.log(userInfo);
-
- delete userInfo.name;
-
- console.log(userInfo);
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popupBg) { // Если цель клика - фот, то:
+      popupBg.classList.remove('active'); // Убираем активный класс с фона
+      popup.classList.remove('active'); // И с окна
+  }
+});
